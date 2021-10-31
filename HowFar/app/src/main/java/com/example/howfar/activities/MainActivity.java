6eec944100939bf98f08267171447e52f984ad0f
Paulo Seoane Davila <paulo.seoane@MacBook-Pro-de-Paulo.local>
2021-10-31 14:35:51 +0100

    added join meet form

--- HowFar/app/src/main/java/com/example/howfar/activities/MainActivity.java
+++ HowFar/app/src/main/java/com/example/howfar/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.howfar.activities;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.howfar.R;
@@ -20,8 +22,12 @@ import com.example.howfar.viewmodels.MainActivityViewModel;
 public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnKeyListener {
     private Button createMeetButton;
     private Button joinMeetButton;
+    private Button goJoinFormButton;
+    private Button cancelJoinFormButton;
     private EditText nameField;
     private TextView helloText;
+    private View tintView;
+    private ConstraintLayout joinMeetForm;
     private MainActivityViewModel viewModel;
 
     @Override
@@ -39,6 +45,14 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
     }
 
     private void initializeViews() {
+        tintView = findViewById(R.id.tintView);
+        tintView.setVisibility(View.GONE);
+        joinMeetForm = findViewById(R.id.joinFormView);
+        joinMeetForm.setVisibility(View.GONE);
+        goJoinFormButton = findViewById(R.id.goJoinFormButton);
+        goJoinFormButton.setOnClickListener(view -> goJoinFormButtonPressed());
+        cancelJoinFormButton = findViewById(R.id.cancelJoinFormButton);
+        cancelJoinFormButton.setOnClickListener(view -> hideJoinForm());
         createMeetButton = findViewById(R.id.createMeetButton);
         createMeetButton.setOnClickListener(view -> createMeetButtonPressed());
         joinMeetButton = findViewById(R.id.joinMeetButton);
@@ -51,6 +65,26 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
         if (viewModel.getNickname().trim() != "") {
             nameField.setText(viewModel.getNickname());
         }
+    }
+
+    private void showJoinForm() {
+        tintView.setVisibility(View.VISIBLE);
+        joinMeetForm.setVisibility(View.VISIBLE);
+        nameField.setEnabled(false);
+        createMeetButton.setEnabled(false);
+        joinMeetButton.setEnabled(false);
+    }
+
+    private void hideJoinForm() {
+        tintView.setVisibility(View.GONE);
+        joinMeetForm.setVisibility(View.GONE);
+        nameField.setEnabled(true);
+        createMeetButton.setEnabled(true);
+        joinMeetButton.setEnabled(true);
+    }
+
+    private void goJoinFormButtonPressed() {
+
     }
 
     private boolean validateNameField() {
@@ -74,7 +108,9 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
     }
 
     private void joinMeetButtonPressed() {
-        validateNameField();
+        if (validateNameField()) {
+            showJoinForm();
+        }
     }
 
     @Override
@@ -100,6 +136,8 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
             InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(nameField.getWindowToken(), 0);
             nameField.clearFocus();
+            joinMeetButton.clearFocus();
+            createMeetButton.clearFocus();
         }
         return false;
     }
