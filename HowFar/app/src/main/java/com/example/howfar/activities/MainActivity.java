72159e799e19208db44ee8f81f79a4f90ee34ac7
Paulo Seoane Davila <paulo.seoane@MacBook-Pro-de-Paulo.local>
2021-10-31 13:21:59 +0100

    added main activity

---
+++ HowFar/app/src/main/java/com/example/howfar/activities/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.howfar.activities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.howfar.R;
+import com.example.howfar.viewmodels.MainActivityViewModel;
+
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnKeyListener {
+    private Button createMeetButton;
+    private Button joinMeetButton;
+    private EditText nameField;
+    private TextView helloText;
+    private MainActivityViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        initializeViews();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        viewModel.activityStopped();
+    }
+
+    private void initializeViews() {
+        createMeetButton = findViewById(R.id.createMeetButton);
+        createMeetButton.setOnClickListener(view -> createMeetButtonPressed());
+        joinMeetButton = findViewById(R.id.joinMeetButton);
+        joinMeetButton.setOnClickListener(view -> joinMeetButtonPressed());
+        nameField = findViewById(R.id.nameField);
+        nameField.addTextChangedListener(this);
+        nameField.setOnKeyListener(this);
+        helloText = findViewById(R.id.helloTextView);
+        setHelloText(viewModel.getNickname());
+        if (viewModel.getNickname().trim() != "") {
+            nameField.setText(viewModel.getNickname());
+        }
+    }
+
+    private boolean validateNameField() {
+        if (nameField.getText().toString().matches("^[a-zA-Z0-9]{4,}$")) {
+            return true;
+        }
+        nameField.setError("Nickname must contain at least 4 non special characters");
+        return false;
+    }
+
+    private void setHelloText(String nickname) {
+        if (nickname.trim() == "") {
+            helloText.setText("Hello!");
+        } else {
+            helloText.setText("Hello " + nickname + "!");
+        }
+    }
+
+    private void createMeetButtonPressed() {
+        validateNameField();
+    }
+
+    private void joinMeetButtonPressed() {
+        validateNameField();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        return;
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        helloText.setText("Hello " + nameField.getText() + "!");
+        viewModel.nicknameChanged(nameField.getText().toString());
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        return;
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        if (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(nameField.getWindowToken(), 0);
+            nameField.clearFocus();
+        }
+        return false;
+    }
+}
---
+++ HowFar/app/src/main/java/com/example/howfar/viewmodels/MainActivityViewModel.java
@@ -0,0 +1,38 @@
+package com.example.howfar.viewmodels;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.howfar.R;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private String userNickname;
+    private SharedPreferences preferences;
+    private String preferencesFile;
+    private final String NICKNAMEKEY = "userName";
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        preferencesFile = application.getString(R.string.shared_preferences_file);
+        preferences = application.getSharedPreferences(preferencesFile, Context.MODE_PRIVATE);
+        userNickname = preferences.getString(NICKNAMEKEY, "");
+        preferences.getString(NICKNAMEKEY, "");
+    }
+
+    public void nicknameChanged(String newNickname) {
+        userNickname = newNickname;
+    }
+    public String getNickname() {
+        return userNickname;
+    }
+    public void activityStopped() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(NICKNAMEKEY, userNickname);
+        editor.apply();
+    }
+}
