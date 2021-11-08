b8eb10ee26379442496efa7a87d6b62eb387e157
Maria <mmp200@gmail.com>
2021-11-08 19:28:32 +0100

    metodo onChecked funciona pero onSensorChanged NO

--- HowFar/app/src/main/java/com/example/howfar/activities/MainActivity.java
+++ HowFar/app/src/main/java/com/example/howfar/activities/MainActivity.java
@@ -2,19 +2,29 @@ package com.example.howfar.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.howfar.R;
@@ -22,9 +32,10 @@ import com.example.howfar.viewmodels.MainActivityViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnKeyListener {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnKeyListener, SensorEventListener {
     private Button createMeetButton;
     private Button joinMeetButton;
     private Button goJoinFormButton;
@@ -36,11 +47,37 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
     private MainActivityViewModel viewModel;
     private List<Place> places = new ArrayList<>();
     private boolean listofcinemasinitialized = false;
+    private SwitchCompat bProx;
+    private SensorManager sensorManager;
+    private Sensor ProxSensor;
+    private TextToSpeech mTTS;
+    private boolean proxSensorActive;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Proximity Sensor to activate Text To Speech
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        ProxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i == TextToSpeech.SUCCESS){
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+                    if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("TTS", "Language not suported");
+                    }else{
+                        //if everything is succesful
+                        mTTS.setPitch(0.5f);
+                        mTTS.setSpeechRate(0.5f);
+                    }
+                }else{
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         initializeViews();
     }
@@ -48,10 +85,59 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
     @Override
     protected void onStop() {
         super.onStop();
+        viewModel.getSensorStatus().observe(this, bool -> publishSensorStatus(bool));
+        viewModel.switchChanged(proxSensorActive);
         viewModel.activityStopped();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        viewModel.getSensorStatus().observe(this, bool -> publishSensorStatus(bool));
+        bProx.setChecked(proxSensorActive);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //sensorManager.registerListener(this, ProxSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //sensorManager.unregisterListener(this, ProxSensor);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent){
+        Toast.makeText(this, "status: main", Toast.LENGTH_SHORT).show();
+        viewModel.onSensorChanged(sensorEvent);
+        viewModel.getSensorStatus().observe(this, bool -> publishSensorStatus(bool));
+        bProx.setChecked(proxSensorActive);
+        mTTS.speak("Introduce your nickname", TextToSpeech.QUEUE_FLUSH, null);
+        //proxSensorActive = viewModel.getSensorStatus();
+        if(proxSensorActive){
+            Toast.makeText(getApplicationContext(), "near", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "far", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void publishSensorStatus(Boolean bool){
+        Toast.makeText(this, "status:" + bool, Toast.LENGTH_SHORT).show();
+        proxSensorActive =  bool;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
 
     private void initializeViews() {
+        bProx = findViewById(R.id.bProx);
+        bProx.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b){
+                viewModel.switchChanged(b);
+            }
+        });
         tintView = findViewById(R.id.tintView);
         tintView.setVisibility(View.GONE);
         tintView.setAlpha(0.0f);
@@ -95,10 +181,15 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
         nameField.setEnabled(true);
         createMeetButton.setEnabled(true);
         joinMeetButton.setEnabled(true);
+        if (proxSensorActive) {
+            mTTS.speak("Cancelling going to the meeting", TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     private void goJoinFormButtonPressed() {
-
+        if (proxSensorActive) {
+            mTTS.speak("Going to the Meeting", TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     private boolean validateNameField() {
@@ -106,6 +197,9 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
             return true;
         }
         nameField.setError("Nickname must contain at least 4 non special characters");
+        if (proxSensorActive) {
+            mTTS.speak("Invalid Nickname", TextToSpeech.QUEUE_FLUSH, null);
+        }
         return false;
     }
 
@@ -118,16 +212,24 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
     }
 
     private void createMeetButtonPressed() {
+        if (proxSensorActive) {
+            mTTS.speak("Creating Meeting", TextToSpeech.QUEUE_FLUSH, null);
+        }
         if (validateNameField()){
             Intent intent = new Intent(this  , CreateMeetActivity.class);
             startActivity(intent);
-
         }
     }
 
     private void joinMeetButtonPressed() {
+        if (proxSensorActive) {
+            mTTS.speak("Join Meeting", TextToSpeech.QUEUE_FLUSH, null);
+        }
         if (validateNameField()) {
             showJoinForm();
+            if (proxSensorActive) {
+                mTTS.speak("Enter ID Meeting", TextToSpeech.QUEUE_ADD, null);
+            }
         }
     }
 
@@ -141,6 +243,9 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
         String name = nameField.getText().toString();
         setHelloText(name);
         viewModel.nicknameChanged(name);
+        if (proxSensorActive) {
+            mTTS.speak(nameField.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     @Override
--- HowFar/app/src/main/java/com/example/howfar/viewmodels/MainActivityViewModel.java
+++ HowFar/app/src/main/java/com/example/howfar/viewmodels/MainActivityViewModel.java
@@ -3,25 +3,57 @@ package com.example.howfar.viewmodels;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 
 import com.example.howfar.R;
+import com.example.howfar.activities.MainActivity;
 
-public class MainActivityViewModel extends AndroidViewModel {
+public class MainActivityViewModel extends AndroidViewModel implements SensorEventListener {
     private String userNickname;
     private SharedPreferences preferences;
     private String preferencesFile;
     private final String NICKNAMEKEY = "userName";
+    private Application application;
+    //private SwitchCompat bProx;
+    private SensorManager sensorManager;
+    private Sensor ProxSensor;
+    //private boolean ProxSensorIsNear = false;
+    private MutableLiveData<Boolean> ProxSensorIsNear;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
+        this.application = application;
+        sensorManager = (SensorManager) application.getSystemService(Context.SENSOR_SERVICE);
+        ProxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
         preferencesFile = application.getString(R.string.shared_preferences_file);
         preferences = application.getSharedPreferences(preferencesFile, Context.MODE_PRIVATE);
         userNickname = preferences.getString(NICKNAMEKEY, "");
         preferences.getString(NICKNAMEKEY, "");
+        preferences.getBoolean("ProxSensorIsNear", false);
+        //bProx.setChecked(ProxSensorIsNear);
+        if(getSensorStatus().getValue()){
+            //If the sensor was on, it is still working
+            //bProx.setVisibility(View.VISIBLE);
+            sensorManager.registerListener(this, ProxSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }else{
+            sensorManager.unregisterListener(this, ProxSensor);
+            //bProx.setVisibility(View.GONE);
+        }
+
     }
 
     public void nicknameChanged(String newNickname) {
@@ -30,9 +62,53 @@ public class MainActivityViewModel extends AndroidViewModel {
     public String getNickname() {
         return userNickname;
     }
+
+    public void switchChanged(boolean b) {
+        if (!b) {
+            // unregister listener and make the appropriate changes in the UI:
+            ProxSensorIsNear.setValue(Boolean.valueOf(b));;
+            sensorManager.unregisterListener(MainActivityViewModel.this, ProxSensor);
+        } else {
+            // register listener and make the appropriate changes in the UI:
+            sensorManager.registerListener(MainActivityViewModel.this, ProxSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            ProxSensorIsNear.setValue(Boolean.valueOf(b));;
+        }
+    }
+
+    public void onSensorChanged(SensorEvent sensorEvent){
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if (sensorEvent.values[0] == 0) { //near
+                ProxSensorIsNear.setValue(Boolean.valueOf(true));
+                //bProx.setVisibility(View.VISIBLE);
+                //bProx.setChecked(ProxSensorIsNear);
+            }
+        }
+    }
+
+
+    //public boolean getSensorStatus(){ return this.ProxSensorIsNear; }
+
+    public MutableLiveData<Boolean> getSensorStatus() {
+        if (ProxSensorIsNear == null) {
+            ProxSensorIsNear = new MutableLiveData<>(false);
+        }
+        return ProxSensorIsNear;
+    }
+
+    public void publishSensorStatus(Boolean bool){
+        ProxSensorIsNear = getSensorStatus();
+        ProxSensorIsNear.postValue(bool);
+    }
+
     public void activityStopped() {
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString(NICKNAMEKEY, userNickname);
+        //ProxSensorIsNear = bProx.isChecked();
+        editor.putBoolean("ProxSensorIsNear", ProxSensorIsNear.getValue());
         editor.apply();
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
